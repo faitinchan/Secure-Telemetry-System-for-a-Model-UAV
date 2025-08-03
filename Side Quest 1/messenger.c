740d950a85d47010a6e9436c21e72f3c02462233
Ho Tin (Timothy) Chan <faitinchan@gmail.com>
2025-08-02 18:38:35 -0700

    Add files via upload

--- Side Quest 1/messenger.c
+++ Side Quest 1/messenger.c
@@ -1,1 +1,33 @@
-kjsdf
+#include <stdio.h>
+
+void run_listener();
+void run_sender(char* dest_ip, char* msg);
+
+int main(int argc, char* argv[]) {
+	if (argc == 1) {
+		printf("[*] Listening for messages...\n");
+		run_listener();
+	} else if (argc == 3) {
+		char* dest_ip = argv[1];
+		char* msg = argv[2];
+		printf("[*] Sending message to %s: \"%s\"\n", dest_ip, msg);
+		run_sender(dest_ip, msg);
+	} else {
+		printf("Usage:\n");
+		printf("  %s                   # Listen for messages\n", argv[0]);
+		printf("  %s <IP> <message>    # Send message to IP\n", argv[0]);
+		return 1;
+	}
+
+	return 0;
+}
+
+void run_listener() {
+	printf("Listened\n");
+	return;
+}
+
+void run_sender(char* dest_ip, char* msg) {
+	printf("Sent\n");
+	return;
+}
